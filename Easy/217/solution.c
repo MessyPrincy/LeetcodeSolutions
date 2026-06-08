cf374dd362d39e069090f537072f9bd5954da8b6
MessyPrincy <alexan2004andw@gmail.com>
2026-06-08 17:44:59 -0400

    28ms, beats 92.22%, 25,26MB, beats 9.20%

--- Easy/217/solution.c
+++ Easy/217/solution.c
@@ -1,3 +1,40 @@
+#define N 5003
+
+typedef struct node
+{
+    int val;
+    struct node *next;
+} node;
+
+node *table[N];
+
+unsigned int hashVal (int val);
+
 bool containsDuplicate(int* nums, int numsSize) {
-    
+    for (int i = 0; i < N; i++) {
+        table[i] = NULL;
+    }
+
+    for (int j = 0; j < numsSize; j++) {
+       unsigned int hashed = hashVal(nums[j]);
+
+        node *ptr = table[hashed];
+        while (ptr != NULL) {
+            if (ptr->val == nums[j]) {
+                return true;
+            }
+            ptr = ptr->next;
+        }
+
+        node *n = malloc(sizeof(node));
+        
+        n->val = nums[j];
+        n->next = table[hashed];
+        table[hashed] = n;
+    }
+    return false;
+}
+
+unsigned int hashVal (int val) {
+    return (val & 0x7FFFFFFF) % N;
 }
