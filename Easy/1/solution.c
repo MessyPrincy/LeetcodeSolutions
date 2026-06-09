c4eff7c330bde2a92ae686b9d5140eec09b1c8dc
MessyPrincy <alexan2004andw@gmail.com>
2026-06-08 22:43:06 -0400

    99ms, beats 78.35%, 9.25MB, beats 93.74%

--- Easy/1/solution.c
+++ Easy/1/solution.c
@@ -2,5 +2,17 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                *returnSize = 2;
+                int *answer = malloc(2 * sizeof(int));
+                answer[0] = i;
+                answer[1] = j;
+                return answer;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
 }
